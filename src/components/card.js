85fe1e511335aad62cea9000463665bd57a5a589
BillyMejia75 <mejiabilly333@gmail.com>
2022-08-10 21:43:39 -0500

    Cambios en las cards

--- src/components/card.js
+++ src/components/card.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 
+import { FaShoppingCart } from "react-icons/fa";
+
 function Card(props) {
   const [state, setState] = useState([]);
 
@@ -7,20 +9,52 @@ function Card(props) {
 
   return (
     <>
-        {console.log("Props", props.data)}
+      {/* {console.log("Props", props.data)}
         <div className="body-card">
         <div className="container">
           <div className="content">
+            <img className="image" src={props.data.codigoBarra} />
             <h1>{props.data.nombre}</h1>
-            <h3>{props.data.categoria}</h3>
-            <h3>{props.data.codigoBarra}</h3>
-            <img src={props.data.codigoBarra} />
             <h3>{props.data.precio}</h3>
           </div>
           <div className="flap"></div>
         </div>
+        </div> */}
+
+      <div id="container">
+        <div className="product-details">
+          <h1>{props.data.nombre}</h1>
+          <span className="hint-star star">
+            <i className="fa fa-star" aria-hidden="true"></i>
+            <i className="fa fa-star" aria-hidden="true"></i>
+            <i className="fa fa-star" aria-hidden="true"></i>
+            <i className="fa fa-star" aria-hidden="true"></i>
+            <i className="fa fa-star-o" aria-hidden="true"></i>
+          </span>
+
+          <p className="information">{props.data.descripcion}</p>
+
+          <div className="control">
+            <button className="btn">
+              <span className="price">${props.data.precio}</span>
+              <span className="shopping-cart">
+                <i className="fa fa-shopping-cart" aria-hidden="true">
+                  <FaShoppingCart />
+                </i>
+              </span>
+              <span className="buy">Get now</span>
+            </button>
+          </div>
         </div>
-      
+
+        {/* <div className="product-image">
+          <img src={props.data.codigoBarra} />
+        </div> */}
+
+        <div className="product-image">
+          <img src={props.data.codigoBarra} />
+        </div>
+      </div>
     </>
   );
 }
